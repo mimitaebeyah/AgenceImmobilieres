832d288bc917dc8836df6969db067f59e4b70ed7
mimitaebeyah <mimitaebeyah@gmail.com>
2022-02-12 20:07:21 +0000

    d

--- AGIM/settings.py
+++ AGIM/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import django_heroku
 import dj_database_url
 import os
-
+ 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -129,7 +129,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-LOGIN_REDIRECT_URL = '/profile/'
+# LOGIN_REDIRECT_URL = '/profile/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
