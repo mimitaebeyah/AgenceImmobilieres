f20c82bcce3e87b6ed48bc9a65fca1216633720e
mimitaebeyah <mimitaebeyah@gmail.com>
2022-02-14 18:47:39 +0000

    m4wd

--- AGIM/settings.py
+++ AGIM/settings.py
@@ -131,26 +131,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-
-    os.path.join(BASE_DIR, "app\static")
-
-]
-
-
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'app\static')
-
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'app\static')
 MEDIA_URL = '/media/'
-
-
-# STATIC_URL = 'static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'app\static')
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = BASE_DIR / 'media'
 # LOGIN_REDIRECT_URL = '/profile/'
 
 # Default primary key field type
