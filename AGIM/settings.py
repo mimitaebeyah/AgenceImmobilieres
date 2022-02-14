33844ce17d6eba7e711bf21a3976ae72c77bfb89
mimitaebeyah <mimitaebeyah@gmail.com>
2022-02-14 12:59:51 +0000

    father

--- AGIM/settings.py
+++ AGIM/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'app\static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 # LOGIN_REDIRECT_URL = '/profile/'
